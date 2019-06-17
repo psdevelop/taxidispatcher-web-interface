f89fa0220d0c7c4c5787e0fa646b857cda7831b2
Stanislav Poltarokov <sp@leads.su>
2019-06-17 15:15:49 +0300

    fix

--- td_app/dispatch_custom.js
+++ td_app/dispatch_custom.js
@@ -1,7 +1,7 @@
 var config = {
 		user: 'disp_server',
 		password: 'disp_server',
-		server: 'localhost\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance
+		server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
 		database: 'TD5R1',
 
 		options: {
